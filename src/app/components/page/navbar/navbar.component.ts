c6cc9c2292a929050d3d6a77b9745a8797d72b7c
Eduardo May <eduardomay_20400@outlook.com>
2019-05-01 18:13:40 -0500

    cerrar sesion

--- src/app/components/page/navbar/navbar.component.ts
+++ src/app/components/page/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthUserService } from 'src/app/services/auth-user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +9,17 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private _authService: AuthUserService) { }
 
   ngOnInit() {
+    this._authService.isAuth().subscribe( userData => console.log(userData));
+  }
+
+  /**
+   * cerrar sesion
+  */
+  public isLogout() {
+    this._authService.logoutUser();
   }
 
 }
