8e725c10a7acdd653d70fdf401174bc23e7e2658
Eduardo May <eduardomay_20400@outlook.com>
2019-05-02 23:06:06 -0500

    #1 obteniendo detalles del usuario logeado

--- src/app/components/page/navbar/navbar.component.ts
+++ src/app/components/page/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthUserService } from 'src/app/services/auth-user.service';
+import { UserInterface } from 'src/app/models/user';
 
 @Component({
   selector: 'app-navbar',
@@ -9,10 +10,25 @@ import { AuthUserService } from 'src/app/services/auth-user.service';
 })
 export class NavbarComponent implements OnInit {
 
+  public statusLoginUser = false;
+  public detailUser: UserInterface = {};
+
   constructor(private router: Router, private _authService: AuthUserService) { }
 
   ngOnInit() {
-    this._authService.isAuth().subscribe( userData => console.log(userData));
+    this._authService.isAuth().subscribe( userData => {
+      if ( userData ) {
+        console.log(userData);
+
+        this.statusLoginUser = true;
+
+        this.detailUser.name = userData.displayName;
+        this.detailUser.id = userData.uid;
+        this.detailUser.photoUrl = userData.photoURL;
+      } else {
+        this.statusLoginUser = false;
+      }
+    });
   }
 
   /**
