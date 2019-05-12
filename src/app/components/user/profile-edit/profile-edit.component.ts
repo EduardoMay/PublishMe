3f76fb47b534b60c5de6de208a9b1a42b561eaba
EduardoMay <eduardo_may@outlook.com>
2019-05-12 15:17:33 -0500

    maquetado de editar perfil

--- src/app/components/user/profile-edit/profile-edit.component.ts
+++ src/app/components/user/profile-edit/profile-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthUserService } from 'src/app/services/auth-user.service';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-edit',
@@ -7,9 +11,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileEditComponent implements OnInit {
 
-  constructor() { }
+  public name = '';
+  public lastName = '';
+  public userName = '';
+  public date = '';
+
+  public switchPersonalDate = true;
+  public switchProfilePhoto = false;
+
+  public statusForm = {
+    name: false,
+    lastName: false,
+    userName: false,
+    date: false
+  };
+
+  private validationText = /([a-zA-Z])/i;
+
+  constructor(private _authService: AuthUserService,
+              private _userServices: UserService,
+              private router: Router) { }
 
   ngOnInit() {
+  }
+
+  public validateFormPersonalDate( formPersonalDate: NgForm ) {
+    console.log(formPersonalDate);
+    if ( formPersonalDate.valid === true) {
+      // funcion para guardar los datos
+
+
+    } else {
+      // Validation input "name"
+      if ( this.name === '' ) {
+        this.statusForm.name = true;
+      } else if ( !this.validationText.test(this.name) ) {
+        this.statusForm.name = true;
+      }
+
+      // Validation input "lastName"
+      if ( this.lastName === '' ) {
+        this.statusForm.lastName = true;
+      } else if ( !this.validationText.test(this.lastName) ) {
+        this.statusForm.lastName = true;
+      }
+
+      // Validation input "username"
+      if ( this.userName === '' ) {
+        this.statusForm.userName = true;
+      } else if ( !this.validationText.test(this.userName) ) {
+        this.statusForm.userName = true;
+      }
+
+      // validation input "date"
+      if ( this.date === '' ) {
+        this.statusForm.date = true;
+      }
+    }
+
+    setTimeout(() => {
+      this.statusForm.name = false;
+      this.statusForm.lastName = false;
+      this.statusForm.userName = false;
+      this.statusForm.date = false;
+    }, 3000);
+  }
+
+  private savePersonalDate() {}
+
+  public buttonPersonalDate() {
+    this.switchPersonalDate = !this.switchPersonalDate;
+    this.switchProfilePhoto = false;
+  }
+
+  public buttonProfilePhoto() {
+    this.switchProfilePhoto = !this.switchProfilePhoto;
+    this.switchPersonalDate = false;
   }
 
 }
