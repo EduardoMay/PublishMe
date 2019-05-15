697eca482bb4ca2405434b35fdf837a9e1a6e4f2
EduardoMay <eduardo_may@outlook.com>
2019-05-15 18:17:19 -0500

    #1 obteniendo la informacion del usuario

--- src/app/components/page/navbar/navbar.component.ts
+++ src/app/components/page/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthUserService } from 'src/app/services/auth-user.service';
 import { UserInterface } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -10,25 +12,53 @@ import { UserInterface } from 'src/app/models/user';
 })
 export class NavbarComponent implements OnInit {
 
-  public statusLoginUser = false;
-  public detailUser: UserInterface = {};
+  public _IDUSER: string;
+  public _INFOUSER: UserInterface = {};
 
-  constructor(private router: Router, private _authService: AuthUserService) { }
+  public statusLoginUser = false;
+
+  constructor(private router: Router,
+    private _authService: AuthUserService,
+    private _userService: UserService) { }
 
   ngOnInit() {
+    this.isAuthUser();
+  }
+
+  /**
+   * Si el usuario ha iniciado sesion
+   */
+  public isAuthUser() {
     this._authService.isAuth().subscribe( userData => {
       if ( userData ) {
-        console.log(userData);
+        // console.log(userData);
+
+        this._IDUSER = userData.uid;
 
         this.statusLoginUser = true;
 
-        this.detailUser.name = userData.displayName;
-        this.detailUser.id = userData.uid;
-        this.detailUser.photoUrl = userData.photoURL;
+        this.getCurrentInfoUser();
       } else {
         this.statusLoginUser = false;
       }
     });
+  }
+
+  /**
+   * Obtener la informacion del usuario
+   */
+  public getCurrentInfoUser() {
+    if ( this._IDUSER !== '' ) {
+      this._userService.getCurrentUser( this._IDUSER ).subscribe( userData => {
+        if (userData) {
+          this._INFOUSER = userData;
+          console.log('info user:', userData);
+        } else {
+          console.error('Error');
+          this._INFOUSER = null;
+        }
+      });
+    }
   }
 
   /**
