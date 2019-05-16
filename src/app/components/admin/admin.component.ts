00494bab1a225344682d25f3e37343125be99b6c
EduardoMay <eduardo_may@outlook.com>
2019-05-16 17:34:53 -0500

    Mostrar informacion

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { UserInterface } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+import { AuthUserService } from 'src/app/services/auth-user.service';
 
 @Component({
   selector: 'app-admin',
@@ -8,18 +11,32 @@ import { NgForm } from '@angular/forms';
 })
 export class AdminComponent implements OnInit {
 
-  public name = '';
-  public lastName = '';
-  public userName = '';
-  public date = '';
+  private _USERID: string;
+  public _DATAUSER: UserInterface = {};
 
-  constructor() { }
+  constructor(private _usersServices: UserService,
+    private _authService: AuthUserService) { }
 
   ngOnInit() {
+    this.isAuth();
   }
 
-  public validateFormPersonalDate( form: NgForm ) {
-    console.log(form);
+  /**
+   * obtener la informacion del usuario logeado
+   */
+  private isAuth() {
+    this._authService.isAuth().subscribe( data => {
+
+      this._USERID = data.uid;
+
+      if ( this._USERID !== '' ) {
+
+        this._usersServices.getCurrentUser(this._USERID).subscribe( dataUser => {
+          this._DATAUSER = dataUser;
+        });
+
+      }
+    });
   }
 
 }
