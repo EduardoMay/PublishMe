c92ade09450e1376568e5ef50bfc2200d0442b9f
EduardoMay <eduardo_may@outlook.com>
2019-05-15 23:15:33 -0500

    #2 mejorando la validacion del formulario

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -16,6 +16,13 @@ export class RegisterComponent implements OnInit {
   public repeatPassword = '';
   public confirmPasswordForm = true;
 
+  public valid = {
+    name: false,
+    email: false
+  };
+  private validationText = /([a-zA-Z])/i;
+  public messageError: string;
+
   constructor(private _authService: AuthUserService, private router: Router) { }
 
   ngOnInit() {
@@ -27,15 +34,39 @@ export class RegisterComponent implements OnInit {
   public confirmPassword(formRegister: NgForm) {
     console.log(formRegister);
 
-    if (formRegister.valid) {
-      if (this.password !== '' && this.repeatPassword !== '') {
+    if ( formRegister.valid ) {
+      if ( this.password !== '' && this.repeatPassword !== '' ) {
         if (this.password === this.repeatPassword) {
           console.log(true);
           this.onRegister();
         } else {
           this.confirmPasswordForm = false;
+          this.messageError = 'Las contraseñas no coiciden';
+          this.password = '';
+          this.repeatPassword = '';
         }
+
+        setTimeout(() => {
+          this.confirmPasswordForm = true;
+        }, 3000);
       }
+    } else {
+      if ( this.name === '' ) {
+        this.valid.name = true;
+      } else if ( !this.validationText.test(this.name) ) {
+        this.valid.name = true;
+      }
+
+      if ( this.email === '' ) {
+        this.valid.email = true;
+      } else if ( !this.validationText.test(this.email) ) {
+        this.valid.email = true;
+      }
+
+      setTimeout(() => {
+        this.valid.name = false;
+        this.valid.email = false;
+      }, 3000);
     }
   }
 
