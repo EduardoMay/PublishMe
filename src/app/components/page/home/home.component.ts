bfca97b65682fb142d91a64d257b48d98e2cdfee
EduardoMay <eduardo_may@outlook.com>
2019-05-21 17:56:31 -0500

    bloquear la tecla enter

--- src/app/components/page/home/home.component.ts
+++ src/app/components/page/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+declare var $: any;
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public textPublication: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.notKeyPressEnter();
+  }
+
+  /**
+   * bloquear la tecla 13/Enter
+   */
+  private notKeyPressEnter() {
+    $('form').keypress( e => {
+      if (e.keyCode === 13) {
+        return false;
+      }
+    });
+  }
+
+  /**
+   * validar el formulario para realizar una publicacion
+   */
+  public validationFormNewPublication(ngForm: NgForm) {
+    console.log(ngForm);
   }
 
 }
