e83f387cd545e4dd5e34cf11d8506a5d488f5f17
EduardoMay <eduardo_may@outlook.com>
2019-05-25 13:49:13 -0500

    validando formulario

--- src/app/components/page/home/home.component.ts
+++ src/app/components/page/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { DataService } from 'src/app/services/data.service';
+import { Publication } from 'src/app/models/publication';
 
 declare var $: any;
 
@@ -12,6 +13,7 @@ declare var $: any;
 export class HomeComponent implements OnInit {
 
   public textPublication: string;
+  public publications: Publication[];
 
   constructor(private _publicationService: DataService) { }
 
@@ -24,14 +26,24 @@ export class HomeComponent implements OnInit {
    */
   public async getAllPublications() {
     const publications = await this._publicationService.getAllPublications();
-    publications.subscribe(data => console.log(data));
+    publications.subscribe(data => this.publications = data);
   }
 
   /**
    * validar el formulario para realizar una publicacion
    */
-  public validationFormNewPublication(ngForm: NgForm) {
-    console.log(ngForm);
+  public validationFormNewPublication(form: NgForm) {
+    if (form.valid === true ) {
+      // save new publication
+    } else {
+      // no text
+
+    }
   }
+
+  /**
+   * save new publication
+   */
+  public saveNewPublication() {}
 
 }
